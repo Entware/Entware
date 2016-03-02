46ea827668593262da5fdc40740a6100e3ca5ff4
zyxmon <zyxmon@gmail.com>
2016-03-02 17:35:59 +0300

    include/golang.mk now saves GO packages sources (with all dependances and patches) to GOARCH... archive in dl folder. If the GO package Makefile defines PKG_SOURCE variable then PKG_SORCE archive is downloaded from http://pkg.entware.net/sources/go and unpacked. `go get` is not used for getting sources, but is used for compilng. No automation for uploading GO archives.

--- include/golang.mk
+++ include/golang.mk
@@ -1,13 +1,31 @@
 STRIP:=:
+ifneq ($(PKG_SOURCE),)
+	PATCH_DIR=
+endif
 
-define Build/Prepare
-	$(INSTALL_DIR) $(PKG_BUILD_DIR)
-	GOPATH=$(PKG_BUILD_DIR) gcc=$(TARGET_CC) ego get -d -x $(PKG_GOGET)
-	$(Build/Patch)
-endef
+ifeq ($(PKG_SOURCE),)
+ define Build/Prepare
+		$(INSTALL_DIR) $(PKG_BUILD_DIR)
+		GOPATH=$(PKG_BUILD_DIR) gcc=$(TARGET_CC) ego get -d -x $(PKG_GOGET)
+		$(Build/Patch)
+ endef
+endif
 
-define Build/Configure
-endef
+ifneq ($(PKG_SOURCE),)
+PKG_SOURCE_URL:=http://pkg.entware.net/sources/go
+ define Build/Prepare
+		$(call Build/Prepare/Default)
+ endef
+endif
+
+ifeq ($(PKG_SOURCE),)
+ define Build/Configure
+	(cd $(PKG_BUILD_DIR); \
+		rm -f $(DL_DIR)/GOARCH-$(PKG_NAME)-$(shell date +%Y-%m-%d).tar.bz ; \
+		tar cjf $(DL_DIR)/GOARCH-$(PKG_NAME)-$(shell date +%Y-%m-%d).tar.bz2 ./src --exclude-vcs ; \
+	)
+ endef
+endif
 
 define Build/Compile
 	GOPATH=$(PKG_BUILD_DIR) gcc=$(TARGET_CC) ego get -x -compiler gccgo -gccgoflags '$(TARGET_GCCGOFLAGS)' $(PKG_GOGET)
