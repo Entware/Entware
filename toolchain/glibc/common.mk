d325c088b85b1b709aaf047d0cabb0aa02ae9379
zyxmon <zyxmon@gmail.com>
2016-03-08 11:26:12 +0300

    glibc 2.23: fix building with ssp enabled

--- toolchain/glibc/common.mk
+++ toolchain/glibc/common.mk
@@ -66,6 +66,7 @@ GLIBC_CONFIGURE:= \
 		--$(if $(CONFIG_SOFT_FLOAT),without,with)-fp
 
 export libc_cv_ssp=no
+export libc_cv_ssp_strong=no
 export ac_cv_header_cpuid_h=yes
 export HOST_CFLAGS := $(HOST_CFLAGS) -idirafter $(CURDIR)/$(PATH_PREFIX)/include
 
