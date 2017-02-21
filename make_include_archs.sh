3ea9bec9ce903cec3d49f93ed79d0ceaa373a055
zyxmon <zyxmon@gmail.com>
2017-02-21 15:01:13 +0300

    update make_include... -> obsolete, use make headers

--- make_include_archs.sh
+++ make_include_archs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-tar czvf bin/armv7-3x-glibc/include.tar.gz -C ./staging_dir/target-arm_cortex-a9_glibc-2.23_eabi/opt/include/ .
-tar czvf bin/armv5-3x-glibc/include.tar.gz -C ./staging_dir/target-arm_xscale_glibc-2.23_eabi/opt/include/ .
-tar czvf bin/x64-3x-glibc/include.tar.gz    -C ./staging_dir/target-x86_64_glibc-2.23/opt/include/ .
-tar czvf bin/mips-3x-glibc/include.tar.gz -C ./staging_dir/target-mips_mips32r2_glibc-2.23/opt/include/ .
-tar czvf bin/mipsel-3x-glibc/include.tar.gz -C ./staging_dir/target-mipsel_mips32r2_glibc-2.23/opt/include/ .
+tar czvf bin/targets/armv7-3x/generic-glibc/packages/include.tar.gz -C ./staging_dir/target-arm_cortex-a9_glibc-2.25_eabi/opt/include/ .
+tar czvf bin/targets/armv5-3x/generic-glibc/packages/include.tar.gz -C ./staging_dir/target-arm_xscale_glibc-2.25_eabi/opt/include/ .
+tar czvf bin/targets/x64-3x/generic-glibc/packages/include.tar.gz    -C ./staging_dir/target-x86_64_glibc-2.25/opt/include/ .
+tar czvf bin/targets/mips-3x/generic-glibc/packages/include.tar.gz -C ./staging_dir/target-mips_mips32r2_glibc-2.25/opt/include/ .
+tar czvf bin/targets/mipsel-3x/generic-glibc/packages/include.tar.gz -C ./staging_dir/target-mipsel_mips32r2_glibc-2.25/opt/include/ .
