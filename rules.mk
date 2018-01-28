52e2c83c535144eb2b137bcefb424839de616696
zyxmon <zyxmon@gmail.com>
2018-01-28 18:02:04 +0300

    rules.mk: fix incorrect include

--- rules.mk
+++ rules.mk
@@ -225,12 +225,12 @@ ifndef DUMP
     export GCC_HONOUR_COPTS:=0
     TARGET_CROSS:=$(if $(TARGET_CROSS),$(TARGET_CROSS),$(OPTIMIZE_FOR_CPU)-openwrt-linux$(if $(TARGET_SUFFIX),-$(TARGET_SUFFIX))-)
     TARGET_CFLAGS+= -fhonour-copts -Wno-error=unused-but-set-variable -Wno-error=unused-result
-    TARGET_CPPFLAGS+= -I$(TOOLCHAIN_DIR)/usr/include
+    TARGET_CPPFLAGS+= -I$(TOOLCHAIN_DIR)/include
     ifeq ($(CONFIG_USE_MUSL),y)
       TARGET_CPPFLAGS+= -I$(TOOLCHAIN_DIR)/include/fortify
     endif
     TARGET_CPPFLAGS+= -I$(TOOLCHAIN_DIR)/include
-    TARGET_LDFLAGS+= -L$(TOOLCHAIN_DIR)/usr/lib -L$(TOOLCHAIN_DIR)/lib
+    TARGET_LDFLAGS+= -L$(TOOLCHAIN_DIR)/lib
     TARGET_PATH:=$(TOOLCHAIN_DIR)/bin:$(TARGET_PATH)
   else
     ifeq ($(CONFIG_NATIVE_TOOLCHAIN),)
