2fde680ba4ee05cf0a03b05ea92274986cf0ad9b
The-BB <tun.chen.bo@gmail.com>
2018-02-12 17:52:22 +0300

    odhcp6c: sync with openwrt, 2018.02

--- package/network/ipv6/odhcp6c/files/dhcpv6.sh
+++ package/network/ipv6/odhcp6c/files/dhcpv6.sh
@@ -11,6 +11,7 @@ proto_dhcpv6_init_config() {
 	proto_config_add_string 'reqprefix:or("auto","no",range(0, 64))'
 	proto_config_add_string clientid
 	proto_config_add_string 'reqopts:list(uinteger)'
+	proto_config_add_string 'defaultreqopts:bool'
 	proto_config_add_string 'noslaaconly:bool'
 	proto_config_add_string 'forceprefix:bool'
 	proto_config_add_string 'extendprefix:bool'
@@ -26,6 +27,7 @@ proto_dhcpv6_init_config() {
 	proto_config_add_string 'ifaceid:ip6addr'
 	proto_config_add_string "userclass"
 	proto_config_add_string "vendorclass"
+	proto_config_add_array "sendopts:list(string)"
 	proto_config_add_boolean delegate
 	proto_config_add_int "soltimeout"
 	proto_config_add_boolean fakeroutes
@@ -38,9 +40,8 @@ proto_dhcpv6_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local reqaddress reqprefix clientid reqopts noslaaconly forceprefix extendprefix norelease ip6prefix iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff
-	json_get_vars reqaddress reqprefix clientid reqopts noslaaconly forceprefix extendprefix norelease ip6prefix iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff
-
+	local reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease ip6prefix iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass sendopts delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff
+	json_get_vars reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease ip6prefix iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff
 
 	# Configure
 	local opts=""
@@ -50,6 +51,8 @@ proto_dhcpv6_setup() {
 	[ "$reqprefix" != "no" ] && append opts "-P$reqprefix"
 
 	[ -n "$clientid" ] && append opts "-c$clientid"
+
+	[ "$defaultreqopts" = "0" ] && append opts "-R"
 
 	[ "$noslaaconly" = "1" ] && append opts "-S"
 
@@ -67,9 +70,17 @@ proto_dhcpv6_setup() {
 
 	[ -n "$ra_holdoff" ] && append opts "-m$ra_holdoff"
 
+	local opt
 	for opt in $reqopts; do
 		append opts "-r$opt"
 	done
+
+	sendopts_cb() {
+		local val="$1"
+		[ -n "$val" ] && append opts "-x$val"
+	}
+
+	json_for_each_item sendopts_cb sendopts
 
 	append opts "-t${soltimeout:-120}"
 
