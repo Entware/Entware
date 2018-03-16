1f58198c95887a1f98852915e4e66dc8832fe130
Alexander Ryzhov <github@ryzhov-al.ru>
2018-03-16 15:25:23 +0300

    dl_check.sh: now can de-duplicate local mirror

--- dl_check.sh
+++ dl_check.sh
@@ -15,3 +15,23 @@ for entware_target in $(ls -1 configs); do
 	fi
     done
 done
+
+mirrors="http://sources.lede-project.org \
+    http://mirror2.openwrt.org/sources \
+    http://downloads.openwrt.org/sources"
+
+echo 'Creating upload mirror...'
+mkdir -p dl.2upload
+cp dl/* dl.2upload
+
+echo 'Deduplicating local mirror...'
+for src in $(ls -1 dl); do
+    for mirror in $mirrors; do
+        wget --quiet --spider $mirror/$src
+        if [ $? -eq 0 ]; then
+            echo "$src found @ $mirror"
+            rm -f "dl.2upload/$src"
+            break
+        fi
+    done
+done
