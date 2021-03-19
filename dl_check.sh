520445f25f6217d9d860fc3331e0067ad56b1708
Alexander Ryzhov <github@ryzhov-al.ru>
2021-03-19 17:43:54 +0300

    Turn on PKG_HASH check

--- dl_check.sh
+++ dl_check.sh
@@ -8,7 +8,7 @@ for entware_target in $(ls -1 configs); do
     echo "Checking $entware_target downloads..."
     cp configs/$entware_target .config
     for openwrt_target in tools toolchain target package; do
-	make -j4 $openwrt_target/download
+	make -j8 $openwrt_target/download V=s
 	if [ $? -ne 0 ]; then
 	    echo "$openwrt_target downloads failed for $entware_target:("
 	    exit 1
@@ -20,6 +20,9 @@ mirrors="https://sources.cdn.openwrt.org
     https://sources.openwrt.org
     https://mirror2.openwrt.org/sources"
 
+exit
+
+# No need to de-duplicate our mirror for now, PKG_MIRROR_HASH should be processed instead of file names
 echo 'Creating upload mirror...'
 mkdir -p dl.2upload
 cp dl/* dl.2upload
