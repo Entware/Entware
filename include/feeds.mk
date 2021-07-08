018696836a9e1a2391942d3771aa16d32143024d
The-BB <tun.chen.bo@gmail.com>
2021-07-08 22:29:36 +0300

    include/feeds.mk: sync with OpenWrt, 2021.07

--- include/feeds.mk
+++ include/feeds.mk
@@ -41,8 +41,14 @@ define FeedSourcesAppend
 ) >> $(1)
 endef
 
-# 1: package name
 # ? fix ABI
+# 1: package name
 #define GetABISuffix
-#$(if $(filter-out kmod-%,$(1)),$(if $(ABIV_$(1)),$(ABIV_$(1)),$(foreach v,$(wildcard $(STAGING_DIR)/pkginfo/$(1).version),$(shell cat $(v)))))
+#$(if $(ABIV_$(1)),$(ABIV_$(1)),$(call FormatABISuffix,$(1),$(foreach v,$(wildcard $(STAGING_DIR)/pkginfo/$(1).version),$(shell cat $(v)))))
+#endef
+
+# 1: package name
+# 2: abi version
+#define FormatABISuffix
+#$(if $(filter-out kmod-%,$(1)),$(if $(2),$(if $(filter %0 %1 %2 %3 %4 %5 %6 %7 %8 %9,$(1)),-)$(2)))
 #endef
