a238289920ed73754865a8583ccee53cefc97b19
The-BB <tun.chen.bo@gmail.com>
2025-05-21 16:47:18 +0300

    include/prereq-build.mk: sync more (cosmetics)

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -186,6 +186,7 @@ $(eval $(call SetupHostCommand,python2,Please install Python 2.7, \
 	python2.7 -V 2>&1 | grep 'Python 2'))
 
 $(eval $(call SetupHostCommand,python,Please install Python >= 3.7, \
+	python3.12 -V 2>&1 | grep 'Python 3', \
 	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
@@ -194,6 +195,7 @@ $(eval $(call SetupHostCommand,python,Please install Python >= 3.7, \
 	python3 -V 2>&1 | grep -E 'Python 3\.([7-9]|[0-9][0-9])\.?'))
 
 $(eval $(call SetupHostCommand,python3,Please install Python >= 3.7, \
+	python3.12 -V 2>&1 | grep 'Python 3', \
 	python3.11 -V 2>&1 | grep 'Python 3', \
 	python3.10 -V 2>&1 | grep 'Python 3', \
 	python3.9 -V 2>&1 | grep 'Python 3', \
